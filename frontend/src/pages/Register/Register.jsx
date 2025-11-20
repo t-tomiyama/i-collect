04e6e66022710bc0d6986180f2890b792416cecf
Thaís Tiemi Tomiyama <t.tomiyama@unesp.br>
2025-11-20 13:24:11 -0300

    Conversão Render ( Vite incompatível )-> Vercel
    Render gera erro MIME para algumas funcionalidades no frontend, Vercel tem suporte React Vite

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { dashboardAPI } from "../services/api";
+import { dashboardAPI } from "../../services/api";
 
 const Dashboard = () => {
   const [user, setUser] = useState(null);
