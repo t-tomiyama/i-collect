11c72f3dd56bacf53f0c3d04f19f994712bff0c8
Thaís Tiemi Tomiyama <t.tomiyama@unesp.br>
2025-11-21 12:21:58 -0300

    Conversão Render ( Vite incompatível )-> Vercel
    Render gera erro MIME para algumas funcionalidades no frontend, Vercel tem suporte React Vite

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -32,9 +32,10 @@ export const NAV_ITEMS = [
     id: "catalog",
     items: [
       { label: "Pesquisar", id: "search", icon: Search },
+      /*
       { label: "Artistas", id: "artists", icon: Music2 },
       { label: "Releases", id: "releases", icon: Disc },
-      { label: "Photocards", id: "pcs", icon: Image },
+      { label: "Photocards", id: "pcs", icon: Image },*/
     ],
   },
   {
